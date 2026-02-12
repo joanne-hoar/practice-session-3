70ed09f970addec50b81d6318140b1ee62a7ce17
Joanne Hoar <joanne.hoar@gmail.com>
2026-02-11 20:36:09 -0700

    add description

--- src/app/services/products-data.service.ts
+++ src/app/services/products-data.service.ts
@@ -12,47 +12,56 @@ export class ProductsDataService {
     {
       id: 1,
       name: "Can of Beans",
-      image: "can-of-beans.jpg"    
+      image: "can-of-beans.jpg",
+      description: "Full of magical moments, this can of beans could be yours!"
     },
     {
       id: 2,
       name: "Bag of Crisps",
-      image: "bag-of-crisps.jpg"
+      image: "bag-of-crisps.jpg",
+      description: "That should say chips. Any flavour you want."
     },
     {
       id: 3,
       name: "Gummy Bears",
-      image: "gummy-bears.jpg"
+      image: "gummy-bears.jpg",
+      description: "Be sure to specify quantity as we have too many and will send you a lot."
     },
     {
       id: 4,
       name: "T-Shirt",
-      image: "t-shirt.jpg"
+      image: "t-shirt.jpg",
+      description: "Wear this t-shirt and you will have good luck."
     },
     {
       id: 5,
       name: "Hobo Bag",
-      image: "hobo-bag.jpg"
+      image: "hobo-bag.jpg",
+      description: "Fits everything you need."
     },
     {
       id: 6,
       name: "Shoes",
       image: "shoes.jpg",
+      description: "Shown one, comes in pairs."
     },
     {
       id: 7,
       name: "Shorts",
-      image: "shorts.jpg"
+      image: "shorts.jpg",
+      description: "Complete the look."
     },
     {
       id: 8,
       name: "Postcard",
-      image: "postcard.jpg"
+      image: "postcard.jpg",
+      description: "Let them know that you're thinking of them."
     },
     {
       id: 9,
       name: "Pens",
-      image: "pens.jpg"
+      image: "pens.jpg",
+      description: "Contains ink."
     }
   ]);
 
