81df4a1260941ebc32a06079d391bb4e899e6809
Joanne Hoar <joanne.hoar@gmail.com>
2026-02-11 20:38:26 -0700

    Part 3 Step 1: Create search-form component

---
+++ src/app/search-form/search-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-form',
+  imports: [],
+  templateUrl: './search-form.html',
+  styleUrl: './search-form.css',
+})
+export class SearchForm {
+
+}
